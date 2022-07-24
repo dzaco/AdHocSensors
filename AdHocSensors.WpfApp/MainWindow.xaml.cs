7026d6214313b829806db0a26c1eeb57ec2cb094
Jacek Giedronowicz <jacek.giedronowicz@gmail.com>
2022-07-24 17:23:11 +0200

    Created WPF Application

---
+++ AdHocSensors.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using AdHocSensors.WpfApp.SettingsViews;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdHocSensors.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenSettingsWindow(object sender, RoutedEventArgs e)
+        {
+            var window = new SettingsWindow();
+            window.Show();
+        }
+    }
+}
