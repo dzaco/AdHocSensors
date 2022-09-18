32b1d16a5e2e8e5f1dcfbb6c3d82f27f8182dd74
Jacek Giedronowicz <jacek.giedronowicz@gmail.com>
2022-09-18 08:26:20 +0200

    renamed project. moved red file class

--- AdHocSensors.WpfApp/Helpers/SensorFileReader.cs
+++ AdHocSensors.WpfApp/Helpers/SensorFileReader.cs
@@ -4,6 +4,7 @@ using AdHocSensors.Domain.FactoryPackage;
 using AdHocSensors.Domain.SettingsPackage;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
